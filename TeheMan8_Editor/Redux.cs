94fb7da275e0d80f86a92814ad6e780326668fd3
PogChampGuy <Kuumba.Lambie@gmail.com>
2024-05-23 17:31:02 -0400

    fixed typo...

--- TeheMan8_Editor/Redux.cs
+++ TeheMan8_Editor/Redux.cs
@@ -15,23 +15,12 @@ namespace TeheMan8_Editor
         #region Methods
         static async internal Task Pause()
         {
-            HttpResponseMessage response = await client.PostAsync("http://127.0.0.1:" + MainWindow.settings.webPort + "/api/v1/execution-flow?function=<pause>&type=-", null);
+            HttpResponseMessage response = await client.PostAsync("http://127.0.0.1:" + MainWindow.settings.webPort + "/api/v1/execution-flow?function=pause", null);
             response.EnsureSuccessStatusCode();
         }
         static async internal Task Resume()
         {
-            HttpResponseMessage response = await client.PostAsync("http://127.0.0.1:" + MainWindow.settings.webPort + "/api/v1/execution-flow?function=<resume>&type=-", null);
-            response.EnsureSuccessStatusCode();
-        }
-        static async internal Task Write(int offset, byte val)
-        {
-            offset &= 0x1FFFFFFF;
-            string url = string.Format("http://127.0.0.1:" + MainWindow.settings.webPort + "/api/v1/cpu/ram/raw?offset={0}&size=1", offset);
-            MultipartFormDataContent content = new MultipartFormDataContent();
-
-            StreamContent fileStreamContent = new StreamContent(new MemoryStream(new byte[] { val }));
-            content.Add(fileStreamContent);
-            var response = await client.PostAsync(url, content);
+            HttpResponseMessage response = await client.PostAsync("http://127.0.0.1:" + MainWindow.settings.webPort + "/api/v1/execution-flow?function=resume", null);
             response.EnsureSuccessStatusCode();
         }
         static async internal Task Write(uint offset, byte val)
@@ -41,18 +30,6 @@ namespace TeheMan8_Editor
             MultipartFormDataContent content = new MultipartFormDataContent();
 
             StreamContent fileStreamContent = new StreamContent(new MemoryStream(new byte[] { val }));
-            content.Add(fileStreamContent);
-            var response = await client.PostAsync(url, content);
-            response.EnsureSuccessStatusCode();
-        }
-        static async internal Task Write(int offset, byte[] vals)
-        {
-            offset &= 0x1FFFFFFF;
-            string url = string.Format(@"http://127.0.0.1:" + MainWindow.settings.webPort + "/api/v1/cpu/ram/raw?offset={0}&size={1}", offset, vals.Length);
-            MultipartFormDataContent content = new MultipartFormDataContent();
-
-
-            StreamContent fileStreamContent = new StreamContent(new MemoryStream(vals));
             content.Add(fileStreamContent);
             var response = await client.PostAsync(url, content);
             response.EnsureSuccessStatusCode();
@@ -93,7 +70,7 @@ namespace TeheMan8_Editor
             var response = await client.PostAsync(url, content);
             response.EnsureSuccessStatusCode();
         }
-        static async internal Task DrawRect(Int32Rect rect,byte[] data)
+        static async internal Task DrawRect(Int32Rect rect, byte[] data)
         {
             string url = string.Format(@"http://127.0.0.1:" + MainWindow.settings.webPort + "/api/v1/gpu/vram/raw?x={0}&y={1}&width={2}&height={3}", rect.X, rect.Y, rect.Width, rect.Height);
             MultipartFormDataContent content = new MultipartFormDataContent();
